cd2b0b0a7163d1e633a781aa9d2cff4946efa97f
JakovJonjić <73243731+Jakov69@users.noreply.github.com>
2023-08-05 18:55:17 +0200

    Add project files.

---
+++ BlazorAppOluja/Server/Data/DataContext.cs
@@ -0,0 +1,42 @@
+﻿namespace BlazorAppOluja.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modeBuilder)
+        {
+            modeBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Knin",
+                    Description = "Knin pobjeda",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ed/16_obljetnica_vojnoredarstvene_operacije_Oluja_05082011_Kninska_tvrdjava_155.jpg",
+                    Price = 9.99m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Mile",
+                    Description = "Predaja Milana Martića",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/hr/4/43/Predaja_21.korpusa_SVK_1995.jpg",
+                    Price = 7.99m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Oluja",
+                    Description = "Operacija Oluja 95",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Map_49_-_Croatia_-_Operation_Oluja%2C_4-8_August_1995.jpg/800px-Map_49_-_Croatia_-_Operation_Oluja%2C_4-8_August_1995.jpg",
+                    Price = 8.99m
+                }
+                );
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazorAppOluja/Server/Migrations/20230805164537_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAppOluja.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Knin pobjeda", "https://upload.wikimedia.org/wikipedia/commons/e/ed/16_obljetnica_vojnoredarstvene_operacije_Oluja_05082011_Kninska_tvrdjava_155.jpg", 9.99m, "Knin" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Predaja Milana Martića", "https://upload.wikimedia.org/wikipedia/hr/4/43/Predaja_21.korpusa_SVK_1995.jpg", 7.99m, "Mile" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Operacija Oluja 95", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Map_49_-_Croatia_-_Operation_Oluja%2C_4-8_August_1995.jpg/800px-Map_49_-_Croatia_-_Operation_Oluja%2C_4-8_August_1995.jpg", 8.99m, "Oluja" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
